5571bd25db079b94fa0f34c243a98e62e9256ee9
Divine Ugorji <divineugorji37@gmail.com>
2025-02-21 15:45:51 +0100

    Add image mask on card hover ✨

--- 03-tailwind-card/src/components/ProfileCard.jsx
+++ 03-tailwind-card/src/components/ProfileCard.jsx
@@ -5,12 +5,15 @@ const ProfileCard = () => {
   return (
     <div className="max-w-4xl bg-white flex flex-col md:flex-row gap-6 py-8 px-4 md:py-16 md:px-6 rounded-sm md:rounded-3xl sm:my-8">
       <div className="rounded-sm">
-        <div className="w-44 h-44">
+        <div className="relative w-44 h-44 overflow-hidden group">
           <img
-            className="w-full h-full object-cover"
+            className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-130"
             src={assets.card_img_1}
             alt=""
           />
+          <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex justify-center items-center">
+            <p className="text-white text-sm font-semibold">Listen Now</p>
+          </div>
         </div>
         <div className="border  border-gray-300 px-2 py-1">
           <h4 className="text-sm font-medium text-black text-wrap">
@@ -30,12 +33,15 @@ const ProfileCard = () => {
       </div>
 
       <div className="rounded-sm">
-        <div className="w-44 h-44">
+        <div className="relative w-44 h-44 overflow-hidden group">
           <img
-            className="w-full h-full object-cover"
+            className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-130"
             src={assets.card_img_2}
             alt=""
           />
+          <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex justify-center items-center">
+            <p className="text-white text-sm font-semibold">Listen Now</p>
+          </div>
         </div>
         <div className="border  border-gray-300 px-2 py-1">
           <h4 className="text-sm font-medium text-black text-wrap">
@@ -55,12 +61,15 @@ const ProfileCard = () => {
       </div>
 
       <div className="rounded-sm">
-        <div className="w-44 h-44">
+        <div className="relative w-44 h-44 overflow-hidden group">
           <img
-            className="w-full h-full object-cover"
+            className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-130"
             src={assets.card_img_3}
             alt=""
           />
+          <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex justify-center items-center">
+            <p className="text-white text-sm font-semibold">Listen Now</p>
+          </div>
         </div>
         <div className="border  border-gray-300 px-2 py-1">
           <h4 className="text-sm font-medium text-black text-wrap">
@@ -80,12 +89,15 @@ const ProfileCard = () => {
       </div>
 
       <div className="rounded-sm">
-        <div className="w-44 h-44">
+        <div className="relative w-44 h-44 overflow-hidden group">
           <img
-            className="w-full h-full object-cover"
+            className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-130"
             src={assets.card_img_4}
             alt=""
           />
+          <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex justify-center items-center">
+            <p className="text-white text-sm font-semibold">Listen Now</p>
+          </div>
         </div>
         <div className="border  border-gray-300 px-2 py-1">
           <h4 className="text-sm font-medium text-black text-wrap">
