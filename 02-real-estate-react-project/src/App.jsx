bee0befbe8fa1f482aaf0a81714a4beba1ff04b8
Divine Ugorji <divineugorji37@gmail.com>
2025-02-23 22:52:25 +0100

    Add testimonials section ✨

--- 02-real-estate-react-project/src/App.jsx
+++ 02-real-estate-react-project/src/App.jsx
@@ -1,11 +1,18 @@
+import React from "react";
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Header from "./components/Header";
+import Projects from "./components/Projects";
+import Testimonials from "./components/Testimonials";
 
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
       <Header />
       <About />
+      <Projects />
+      <Testimonials />
+      <Contact />
     </div>
   );
 };
--- 02-real-estate-react-project/src/components/Header.jsx
+++ 02-real-estate-react-project/src/components/Header.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Navbar from "./navbar";
 
 const Header = () => {
@@ -12,7 +13,7 @@ const Header = () => {
         <h2 className="text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20">
           Explore Homes that Fit Your Dreams
         </h2>
-        <div className="space-x-16 mt-16">
+        <div className="flex flex-col gap-4 xs:flex-row justify-center xs:gap-16 mt-16">
           <a href="#Projects" className="border border-white px-8 py-3 rounded">
             projects
           </a>
