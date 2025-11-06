472289ebcb079ce4ff68cfb3748eee217ee9f9b1
Divine Ugorji <divineugorji37@gmail.com>
2025-11-06 21:53:16 +0100

    next-saas-page: imlement logo ticker 🎉

---
+++ 07-next-saas-page/src/components/LogoTicker.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import acmeLogo from "../assets/images/acme.png";
+import quantumLogo from "../assets/images/quantum.png";
+import echoLogo from "../assets/images/echo.png";
+import celestialLogo from "../assets/images/celestial.png";
+import pulseLogo from "../assets/images/pulse.png";
+import apexLogo from "../assets/images/apex.png";
+
+const images = [
+  { src: acmeLogo, id: 1 },
+  { src: quantumLogo, id: 2 },
+  { src: echoLogo, id: 3 },
+  { src: celestialLogo, id: 4 },
+  { src: pulseLogo, id: 5 },
+  { src: apexLogo, id: 6 },
+];
+
+export const LogoTicker = () => {
+  return (
+    <div className="bg-black text-white py-[72px] sm:py-24">
+      <div className="container">
+        <h2 className="text-xl text-center text-white/70">
+          Trusted by the world&apos most innovative teams
+        </h2>
+        <div
+          className="overflow-hidden mt-9 before:content-[''] 
+        after:content-[''] before:absolute after:absolute before:h-full 
+        after:h-full before:w-5 after:w-5 relative
+        before:left-0 after:right-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right, #000, rgb(0,0,0,0))] after:bg-[linear-gradient(to_right, #000, rgb(0,0,0,0))]"
+        >
+          <div className="flex gap-16 ">
+            {images.map((imageSrc) => (
+              <Image
+                src={imageSrc.src}
+                alt=""
+                key={imageSrc.id}
+                className="flex-none h-8 w-auto"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
