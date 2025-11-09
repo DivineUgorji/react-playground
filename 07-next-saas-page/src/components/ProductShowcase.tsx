22ce580e7a99bb647a0e17008396b7674d8ba83b
Divine Ugorji <divineugorji37@gmail.com>
2025-11-09 08:22:37 +0100

    next-saas-page: animate product showcase component 🎉

--- 07-next-saas-page/src/components/ProductShowcase.tsx
+++ 07-next-saas-page/src/components/ProductShowcase.tsx
@@ -1,7 +1,19 @@
+"use client";
+
 import Image from "next/image";
 import appScreen from "../assets/images/app-screen.png";
+import { motion, useScroll, useTransform } from "motion/react";
+import { useEffect, useRef } from "react";
 
 export const ProductShowcase = () => {
+  const appImageRef = useRef<HTMLImageElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: appImageRef,
+    offset: ["start end", "end end"],
+  });
+
+  const rotateX = useTransform(scrollYProgress, [0, 1], [15, 0]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
   return (
     <div className="bg-black text-white text-center bg-linear-to-b from-black to-[#5D2CA8] py-[72px] sm:py-24">
       <div className="container">
@@ -16,7 +28,20 @@ export const ProductShowcase = () => {
           </p>
         </div>
         <div className="inline-flex">
-          <Image src={appScreen} alt="product screenshot" className="mt-14" />
+          <motion.div
+            style={{
+              opacity: opacity,
+              rotateX: rotateX,
+              transformPerspective: "800px",
+            }}
+          >
+            <Image
+              src={appScreen}
+              alt="product screenshot"
+              className="mt-14"
+              ref={appImageRef}
+            />
+          </motion.div>
         </div>
       </div>
     </div>
